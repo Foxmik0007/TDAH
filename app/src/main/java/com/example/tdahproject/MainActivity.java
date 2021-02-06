41862239d014fc51300dcbce8990e664d26bafde
Foxmik0007 <mrafa099@uottawa.ca>
2021-02-06 15:40:55 -0200

    Version 0.1

--- app/src/main/java/com/example/tdahproject/MainActivity.java
+++ app/src/main/java/com/example/tdahproject/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.tdahproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NavController myController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        bottomNavigationView = (BottomNavigationView) findViewById( R.id.bottomNavigationView );
+        myController = Navigation.findNavController(this, R.id.fragment );
+
+
     }
 }
